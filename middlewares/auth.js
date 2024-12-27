1fedd555cd115bca4e9eb3c55ae442c8c2df4358
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2024-12-27 23:32:14 +0500

    jwt token values

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 // Sign JWT Token
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id, name, email, password, userType, organizationName, contactPerson, location, registrationID, areasServed, typesOfWasteManaged,phone,score }, process.env.JWT_SECRET, {
     expiresIn: '1d', // Set expiration to 1 days
   });
 };
